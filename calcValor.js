956370c4be4c59a75b74e8a0375a804482441e2e
Bruno Saad <saadbruno@gmail.com>
2024-09-10 13:00:06 -0300

    calculo correto de desconto para filiados

--- calcValor.js
+++ calcValor.js
@@ -1,15 +1,27 @@
 valorFinal.onkeyup = function(){
+    calcValores();
+};
+
+valorFiliadosFinal.onkeyup = function(){
+    calcValores();
+};
+
+function arredondar(value) {
+    return Math.round(value * 100)/100;
+}
+
+function calcValores() {
     console.log(valorFinal.value);
 
-
     // ticketsports fica com 10% do valor
+    // vamos supor:
     const valorDescontado = valorFinal.value / 1.1;
-    const valorDescontadoFiliados = (valorFinal.value - 100) / 1.1;
+    const valorDescontadoFiliados = valorFiliadosFinal.value / 1.1;
 
-    const valorDescontoFiliados = valorFinal.value - valorDescontadoFiliados;
+    const valorDescontoFiliados = valorDescontado - valorDescontadoFiliados;
 
-    valorTS.innerHTML = valorDescontado;
-    valorFiliadosTS.innerHTML = valorDescontoFiliados;
+    valorTS.innerHTML = arredondar(valorDescontado);
+    valorFiliadosTS.innerHTML = arredondar(valorDescontoFiliados);
 
 
-};
+}
